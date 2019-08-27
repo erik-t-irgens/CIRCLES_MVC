8503643aa45f9a29e47c8fcef87555e4d07e6aeb
Student <example@example.com>
2019-08-27 10:25:20 -0700

    Add CRUD for Circles to completion.

--- Controllers/CirclesController.cs
+++ Controllers/CirclesController.cs
@@ -10,30 +10,68 @@ namespace Circles_MVC.Controllers
 {
     public class CirclesController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Edit()
-        {
-            return View();
-        }
-
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        public IActionResult Details()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Errors()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        public IActionResult Index()
+        {
+            var allCircles = Circle.GetAllCircles();
+            return View(allCircles);
+        }
+
+        // public IActionResult IndexNext()
+        // {
+        //     var nextDestinations = Destination.GetNextDestinations();
+        //     return View("Index", nextDestinations);
+        // }
+
+        // public IActionResult IndexPrev()
+        // {
+        //     var prevDestinations = Destination.GetPrevDestinations();
+        //     return View("Index", prevDestinations);
+        // }
+
+        public IActionResult Details(int id)
+        {
+            var thisCircle = Circle.GetThisCircle(id);
+            return View(thisCircle);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Circle circle)
+        {
+            Circle.CreateCircle(circle);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Circle.DeleteCircle(id);
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Edit(int id)
+        {
+            var particularCircle = Circle.GetThisCircle(id);
+            return View(particularCircle);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, Circle circle)
+        {
+            Circle.EditCircle(id, circle);
+            return RedirectToAction("Index");
         }
     }
 }
--- Models/Circle.cs
+++ Models/Circle.cs
@@ -2,10 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace Circles_MVC.Models
 {
-        public class Circle
+    public class Circle
     {
         public int CircleId { get; set; }
         public string Name { get; set; }
@@ -13,5 +18,115 @@ namespace Circles_MVC.Models
 
         public ICollection<CircleUserprofile> Userprofiles { get; set; }
         public virtual ApplicationUser Creator { get; set; }
+
+        public static List<Circle> GetAllCircles()
+        {
+            var client = new RestClient("http://localhost:5000/api/");
+            var request = new RestRequest("circles", Method.GET);
+            var response = new RestResponse();
+
+            Task.Run(async () =>
+            {
+                response = await GetAsyncResponse.GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+
+            JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(response.Content);
+            var circleList = JsonConvert.DeserializeObject<List<Circle>>(jsonResponse.ToString());
+            return circleList;
+        }
+
+        // Display next page of circles
+        // public static List<Circle> GetNextCircles()
+        // {
+        //     var client = new RestClient("http://localhost:5000/api/");
+        //     var request = new RestRequest("circles/next", Method.GET);
+        //     var response = new RestResponse();
+
+        //     Task.Run(async () =>
+        //     {
+        //         response = await GetAsyncResponse.GetResponseContentAsync(client, request) as RestResponse;
+        //     }).Wait();
+
+        //     JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(response.Content);
+        //     var circleList = JsonConvert.DeserializeObject<List<Circle>>(jsonResponse.ToString());
+        //     return circleList;
+        // }
+
+        // // Display prev page of circles
+        // public static List<Circle> GetPrevCircles()
+        // {
+        //     var client = new RestClient("http://localhost:5000/api/");
+        //     var request = new RestRequest("circles/prev", Method.GET);
+        //     var response = new RestResponse();
+
+        //     Task.Run(async () =>
+        //     {
+        //         response = await GetAsyncResponse.GetResponseContentAsync(client, request) as RestResponse;
+        //     }).Wait();
+
+        //     JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(response.Content);
+        //     var circleList = JsonConvert.DeserializeObject<List<Circle>>(jsonResponse.ToString());
+        //     return circleList;
+        // }
+
+        // Display particular circle 
+        public static Circle GetThisCircle(int id)
+        {
+            var client = new RestClient("http://localhost:5000/api/");
+            var request = new RestRequest("circles/" + id, Method.GET);
+            var response = new RestResponse();
+
+            Task.Run(async () =>
+            {
+                response = await GetAsyncResponse.GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            var thisCircle = JsonConvert.DeserializeObject<Circle>(jsonResponse.ToString());
+            return thisCircle;
+        }
+
+        // Add new Circle
+        public static void CreateCircle(Circle circle)
+        {
+            var client = new RestClient("http://localhost:5000/api/");
+            var request = new RestRequest("circles", Method.POST);
+            request.AddJsonBody(circle);
+            var response = new RestResponse();
+
+            Task.Run(async () =>
+            {
+                response = await GetAsyncResponse.GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+        }
+
+        // Delete particular Circle
+        public static void DeleteCircle(int id)
+        {
+            var client = new RestClient("http://localhost:5000/api/");
+            var request = new RestRequest("circles/" + id, Method.DELETE);
+            var response = new RestResponse();
+
+            Task.Run(async () =>
+            {
+                response = await GetAsyncResponse.GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+        }
+
+        // Editing a particular Circle
+        public static void EditCircle(int id, Circle circle)
+        {
+            var client = new RestClient("http://localhost:5000/api/");
+            var request = new RestRequest("circles/" + id, Method.PUT);
+            request.AddJsonBody(circle);
+            var response = new RestResponse();
+
+            Task.Run(async () =>
+            {
+                response = await GetAsyncResponse.GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+        }
+
+
     }
 }
